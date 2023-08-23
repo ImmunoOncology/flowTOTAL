9c555af94a911f32c69f9d69452be8bf0c44357e
Juanlu Onieva <juanlu@MacBook-Pro-de-Juanlu-2.local>
2023-08-23 16:43:06 +0200

    Run flowTOTAL

--- R/runFlowTOTAL.R
+++ R/runFlowTOTAL.R
@@ -1,32 +1,5 @@
-library(flowCore)
 
-source("/Volumes/TRANSCEND/GitHub/flowTOTAL/R/helperFunctions.R")
-source("/Volumes/TRANSCEND/GitHub/flowTOTAL/R/runPreprocessing.R")
-source("/Volumes/TRANSCEND/GitHub/flowTOTAL/R/runBackgating.R")
-source("/Volumes/TRANSCEND/GitHub/flowTOTAL/R/estimateProportion.R")
 
-fcs_path <- "/Volumes/ExtremeSSD/IBIMA/Citometria-IBIMA/fcs_raw-civil--11-07-2023"
-output <- "/Volumes/TRANSCEND/IBIMA/Citometria/flowTOTAL-pruebas"
-
-metadata <- data.frame(filename=list.files(fcs_path, full.names = T))
-metadata$file <-sapply(strsplit(metadata$filename, "/"), function(x) x[length(x)]) 
-metadata$filename_clean <- paste0(output, "/fcs_clean/", metadata$file)
-
-message("Step1: runPreprocessing()")
-# runPreprocessing(metadata, output = output, report = T, ncores = NULL)
-
-message("Step2: runDensityBackgating()")
-metadata <- metadata[grep("Panel1", metadata$filename), ]
-# runDensityBackgating(metadata = metadata, output = output, channel_bg = c("PE-Cy7-A+", "SSC-A-"))
-
-message("Step3: runDensityBackgating()")
-info_panel <- read.delim("/Volumes/TRANSCEND/IBIMA/Citometria/flowTOTAL-pruebas/Info_panel.txt")
-log_file_track <- "/Volumes/TRANSCEND/IBIMA/Citometria/flowTOTAL-pruebas/Log_file_track.txt"
-runEstimateProprotion(log_file_track, info_panel, output, ncores=NULL)
-
-  
-
-  
 runFlowTOTAL <- function(fcs_path, output, panel_backgating=NULL, panel_estimate=NULL){
 
   metadata <- data.frame(filename=list.files(fcs_path, full.names = T))
