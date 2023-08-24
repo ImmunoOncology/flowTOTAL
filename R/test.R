47256d06c137891925dcb25c0f1c8178f18054e4
Juanlu Onieva <juanlu@MacBook-Pro-de-Juanlu-2.local>
2023-08-24 09:02:47 +0200

    Test comment code

--- R/test.R
+++ R/test.R
@@ -25,9 +25,10 @@
 # runEstimateProprotion(log_file_track, info_panel, output, ncores=NULL)
 # 
 
-library(flowTOTAL)
-runFlowTOTAL("/Volumes/TRANSCEND/IBIMA/Citometria/flowTOTAL-pruebas/fcs_raw", 
-             "/Volumes/TRANSCEND/IBIMA/Citometria/flowTOTAL-pruebas", 
-             panel_backgating=c("PE-Cy7-A+", "SSC-A-"), 
-             panel_estimate="/Volumes/TRANSCEND/IBIMA/Citometria/flowTOTAL-pruebas/Info_panel.txt"
-            )
+# remotes::install_github(repo = "ImmunoOncology/flowTOTAL", ref = "dev-1.0.0")
+# library(flowTOTAL)
+# runFlowTOTAL("/Volumes/TRANSCEND/IBIMA/Citometria/flowTOTAL-pruebas/fcs_raw", 
+#              "/Volumes/TRANSCEND/IBIMA/Citometria/flowTOTAL-pruebas", 
+#              panel_backgating=c("PE-Cy7-A+", "SSC-A-"), 
+#              panel_estimate="/Volumes/TRANSCEND/IBIMA/Citometria/flowTOTAL-pruebas/Info_panel.txt"
+#             )
